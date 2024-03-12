68375076cec0bff0cbb61ef6d028395ed4105297
Igor do Espírito Santo Balbino <igor.balbino@teknisa.com>
2024-03-12 14:03:12 -0300

    Background do jogo implementado mas ainda não funiona direito

--- README.md
+++ README.md
@@ -3,9 +3,13 @@
 Será um joguinho legal de plataforma baseado num tutorial do youtube: `https://youtu.be/rTVoyWu8r6g?si=T7NRbVgJMIOsgJ43`.
 Feito em HTML, CSS e JavaScript, usando Canva.
 
-Assets: `https://craftpix.net/freebies/`, `https://www.gameart2d.com/freebies.html`, `https://www.freepik.com/free-photos-vectors/game-asset`, `https://gameassetsfree.com/`, `https://opengameart.org/`, `https://itch.io/game-assets/free`
+Assets: `https://drive.google.com/drive/folders/1hqYFTOvtzxQQuFZPJjzUOOGjzAfEFEio`, `https://craftpix.net/freebies/`, `https://www.gameart2d.com/freebies.html`, `https://www.freepik.com/free-photos-vectors/game-asset`, `https://gameassetsfree.com/`, `https://opengameart.org/`, `https://itch.io/game-assets/free`
 
 # Autores
 
  - Igor do E. S. Balbino
  - David Gomez
+
+# Changelog
+
+12/03/2024 - Background do jogo implementado mas ainda não funiona direito. Base com os assets: `https://drive.google.com/drive/folders/1hqYFTOvtzxQQuFZPJjzUOOGjzAfEFEio`
--- index.js
+++ index.js
@@ -9,9 +9,39 @@ const keys = {
         pressed: false
     }
 };
+const scaledCanvas = {
+    width: canvas.width / 4,
+    height: canvas.height / 4
+};
 
 canvas.width = 1024; //largura da tela do jogo
 canvas.height = 576; //alura da tela do jogo
+
+class Sprite {
+    constructor({ position, imgSrc }) {
+        this.position = position;
+        this.image = new Image(); //cria var local e instancia classe Image
+        this.image.src = imgSrc; //seta o valor de source da imagem do sprite
+    }
+
+    draw() {
+        if(!this.image) return; //se a imagem estiver vazia, retorna sem fazer nada
+        c.drawImage(this.image, this.position.x, this.position.y);
+    }
+
+    update() {
+        this.draw();
+    }
+}
+
+//INSTANCIA CLASSE SPRITE. INSTANCIAMENTOS DEVEM FICAR DEPOIS DA CRIAÇÃO DA CLASSE
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imgSrc: './assets/background.png'
+});
 
 class Player {
     constructor (position) {
@@ -20,32 +50,38 @@ class Player {
             x: 0,
             y: 1
         };
-        this.heigth = 100;
+        this.height = 100;
     } //construtor da classe player. sempre executa quando cria um novo player
 
     draw() {
         c.fillStyle = 'red'; //cor do objeto
-        c.fillRect(this.position.x, this.position.y, 100, this.heigth); //argumentos: x, y, width, heigth
+        c.fillRect(this.position.x, this.position.y, 100, this.height); //argumentos: x, y, width, height
     }
 
     update() {
         this.draw(); //refaz os desenhos quando entra no loop
         this.position.x += this.velocity.x; //aumenta a posicao de forma q faz o obj andar
         this.position.y += this.velocity.y; //aumenta a posicao de forma q faz o obj cair
-        if((this.position.y + this.heigth + this.velocity.y) < canvas.height)
+        if((this.position.y + this.height + this.velocity.y) < canvas.height)
             this.velocity.y += gravity; // se a posicao do player, mais o tamanho dele, mais a velocidade na qual ele estava cainto no eixo y for menor q o tamanho da tela, para de cair.
         else this.velocity.y = 0; // colocamos a velocidade em zero para que pare de contar no eixo y
     }
 }
 
+//INSTANCIA CLASSE SPRITE. INSTANCIAMENTOS DEVEM FICAR DEPOIS DA CRIAÇÃO DA CLASSE
 const player = new Player({x: 10, y: 10}); //instancia o player
 
 function animate() {
     window.requestAnimationFrame(animate); //atualiza o frame da tela com o argumento criando um loop
 
     c.fillStyle = 'white'; //cor do objeto
-    c.fillRect(10, 10, canvas.width, canvas.height); //argumentos: x, y, width, heigth
+    c.fillRect(0, 0, canvas.width, canvas.height); //argumentos: x, y, width, height
 
+    c.save(); //saves the entire state of the canvas by pushing the current state onto a stack. link: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/save
+    c.scale(4, 4); //adds a scaling transformation to the canvas units horizontally and/or vertically. link: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/scale
+    c.translate(0, -background.image.height + scaledCanvas.height); //adds a translation transformation to the current matrix. link: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/translate
+    background.update();
+    c.restore(); // restores the most recently saved canvas state by popping the top entry in the drawing state stack. If there is no saved state, this method does nothing. link: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/restore
     player.update(); //da update no player para q se mova
 
     player.velocity.x = 0;
